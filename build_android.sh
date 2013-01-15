31b7edd61ac382cbb631c91c2b10645b5dee340b
Norbert Schultz <nosc@cgvis.de>
2013-01-15 15:33:23 +0100

    multi debug,release build

--- build_android.sh
+++ build_android.sh
@@ -5,6 +5,9 @@ cd $DIRNAME
 mkdir -p build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake  -DANDROID_NATIVE_API_LEVEL=9 -DLIBRARY_OUTPUT_PATH_ROOT=. -DBOOST_ROOT=/usr/local/boost/ -DCMAKE_INSTALL_PREFIX=../install
-make -j4
-make install
+cmake . -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=../install/arm_debug
+make -j4 install
+cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=../install/arm_release
+make -j4 install
+
 
