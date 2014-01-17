a045e8cd09217d7e161f5fad5ab204eefbc4d7a7
Norbert Schultz <nosc@cgvis.de>
2014-01-17 13:20:50 +0100

    some improvements on android build

--- build_android.sh
+++ build_android.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e # fail on first error
 DIRNAME=`dirname $0`
+BOOST_ROOT=/usr
 cd $DIRNAME
 mkdir -p build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake  -DANDROID_NATIVE_API_LEVEL=9 -DLIBRARY_OUTPUT_PATH_ROOT=. -DBOOST_ROOT=/usr/local/boost/ -DCMAKE_INSTALL_PREFIX=../install
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake  -DANDROID_NATIVE_API_LEVEL=9 -DLIBRARY_OUTPUT_PATH_ROOT=. -DBOOST_ROOT=$BOOST_ROOT -DCMAKE_INSTALL_PREFIX=../install
 cmake . -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=../install/arm_debug
-make -j4 install
+make install
 cmake . -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=../install/arm_release
-make -j4 install
+make install
 
 
